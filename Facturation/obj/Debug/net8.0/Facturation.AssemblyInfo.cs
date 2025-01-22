1cc8e776439a8e3716c862ba67a6b853b39da574
Dhouibi Mohamed Aziz <mohamedaziz.dhouibi@insat.ucar.tn>
2025-01-22 15:59:00 +0100

    ..

--- Facturation/obj/Debug/net8.0/Facturation.AssemblyInfo.cs
+++ Facturation/obj/Debug/net8.0/Facturation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Facturation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de36d4317e45f841cb6fb27d19f6194d47bc7d17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc78021644d4491fefe9a9572e256b90c789e1be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Facturation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Facturation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
