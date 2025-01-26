7c5af821da21093fca9eba097d498398c99aac52
Dhouibi Mohamed Aziz <mohamedaziz.dhouibi@insat.ucar.tn>
2025-01-26 21:13:12 +0100

    update ReserverProduitDTO

--- Persistence/DTO/GestionStock/ReserverProduitDTO.cs
+++ Persistence/DTO/GestionStock/ReserverProduitDTO.cs
@@ -5,8 +5,3 @@ public class ReserverProduitRequestDTO : ArticleExpedierMarchandisesDTO
     public string ReservationDuration { get; init; }
     public int CommandeId { get; init; }
 }
-
-public class ReserverProduitResponseDTO
-{
-    public string ReservationId { get; init; }
-}
