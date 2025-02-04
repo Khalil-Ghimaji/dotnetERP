093ff3f712d73c9ec4909767d96c9bff71e42e80
Rayen-Chemlali <155291708+Rayen-Chemlali@users.noreply.github.com>
2025-02-04 23:32:43 +0100

    Update CreerFactureDTO.cs

--- Persistence/DTO/Facturation/CreerFactureDTO.cs
+++ Persistence/DTO/Facturation/CreerFactureDTO.cs
@@ -5,11 +5,7 @@ namespace Persistence.DTO.Facturation
     public class CreerFactureDTO
     {
         public int CommandeId { get; set; }
-        public DateTime DateGeneration { get; set; } = DateTime.Now;
-        
-        public float? MontantTotal { get; set; }
-        public StatusFacture StatusFacture { get; set; } = StatusFacture.Créée;
-        public float MontantPayé { get; set; } = 0;
+
 
     }
 }
