dd6742f212f1954d535ec3105336c68849d589a3
Rayen-Chemlali <155291708+Rayen-Chemlali@users.noreply.github.com>
2025-02-04 23:32:57 +0100

    Update CreerEcheanceDTO.cs

--- Persistence/DTO/Facturation/CreerEcheanceDTO.cs
+++ Persistence/DTO/Facturation/CreerEcheanceDTO.cs
@@ -4,11 +4,7 @@ namespace Persistence.DTO.Facturation
 {
     public class CreerEcheanceDTO
     {
-        public DateTime Date { get; set; } = DateTime.Now;
         public float Montant { get; set; }
         public MethodePaiement MethodePaiement { get; set; }
-        public StatutEcheance StatutEcheance { get; set; } = StatutEcheance.Impayée;
-
-
     }
 }
